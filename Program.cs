35bc79709f82282dc251400e169df0307850c2bd
Chris Stockdale <cjsgamingnetwork92@gmail.com>
2020-10-04 02:09:05 -0500

    Comments Added

--- Program.cs
+++ Program.cs
@@ -8,11 +8,16 @@ namespace Number_Guesser
     {
         static void Main(string[] args)
         {
+            // Generate A Random Number For The Guess
             Random r = new Random();
+            // Initialization Of The Correct Answer
             int correctAnswer = r.Next(0, 100);
+            // Initialization Of The Win Variable
             bool win = false;
+            // Initialization Of The Thank You Message
             String thankYouMsg = "Thank you for playing";
 
+            // Loop To Keep Guessing The Answer
             do
             {
                 Console.Write("Guess a number between 0 and 100: ");
@@ -35,6 +40,7 @@ namespace Number_Guesser
                 }
 
                 Console.WriteLine();
+                // Continuous Loop While Win Is False
             } while (win == false);
             {
 
